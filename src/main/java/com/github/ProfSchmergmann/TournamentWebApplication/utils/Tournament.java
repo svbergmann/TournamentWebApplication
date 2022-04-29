0b8b9beadecedd1a2110c67aa2ab739598614b1c
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2022-04-29 11:47:01 +0200

    created a tournament class for creating all tournament methods.

---
+++ src/main/java/com/github/ProfSchmergmann/TournamentWebApplication/utils/Tournament.java
@@ -0,0 +1,14 @@
+package com.github.ProfSchmergmann.TournamentWebApplication.utils;
+
+import com.github.ProfSchmergmann.TournamentWebApplication.database.models.game.Game;
+import com.github.ProfSchmergmann.TournamentWebApplication.database.models.team.Team;
+
+import java.util.List;
+
+public class Tournament {
+
+	public static List<Game> getRoundRobinGames(List<Team> teams) {
+
+	}
+
+}
